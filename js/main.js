b65fe6a592b9586b707695a010448cfb6510c12a
HypitoM <boba.zdez@yandex.ru>
2022-08-08 15:13:14 +0300

    fix slider and counter

--- js/main.js
+++ js/main.js
@@ -1,20 +1,5 @@
 $(function(){
 
-    // $('.heroes__slider-img').slick({
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1,
-    //     asNavFor: '.heroes__slider-text',
-    //
-    //
-    // });
-    //
-    // $('.heroes__slider-text').slick({
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1,
-    //     asNavFor: '.heroes__slider-img',
-    //     fade: true,
-    //     arrows: false,
-    // });
 
     $('.heroes__slider-img').slick({
         slidesToShow: 1,
