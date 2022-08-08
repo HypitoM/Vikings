aed9dd180cc2e3c508075394955e95592b3e486b
HypitoM <boba.zdez@yandex.ru>
2022-08-08 14:58:59 +0300

    fix slider

--- js/main.js
+++ js/main.js
@@ -1,19 +1,35 @@
 $(function(){
 
+    // $('.heroes__slider-img').slick({
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     asNavFor: '.heroes__slider-text',
+    //
+    //
+    // });
+    //
+    // $('.heroes__slider-text').slick({
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     asNavFor: '.heroes__slider-img',
+    //     fade: true,
+    //     arrows: false,
+    // });
+
     $('.heroes__slider-img').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        asNavFor: '.heroes__slider-text',
         prevArrow: '<button class="slick-btn slick-prev">' +
             '<img src="images/arrow-left.png" alt="prev"></button>',
         nextArrow: '<button class="slick-btn slick-next">' +
-            '<img src="images/arrow-right.png" alt="next"></button>'
+            '<img src="images/arrow-right.png" alt="next"></button>',
+        asNavFor: '.heroes__slider-text'
     });
     $('.heroes__slider-text').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         asNavFor: '.heroes__slider-img',
-        fade: true,
+        focusOnSelect: true,
         arrows: false
     });
 
